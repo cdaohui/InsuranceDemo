67dddaa7ab1c8fba71eeb5689dfe52346384792b
cdh <cdh@cdhdemac-mini-2.local>
2016-02-24 11:27:45 +0800

    58556

---
+++ INDEMO/INDEMO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  INDEMO
+//
+//  Created by cdh on 16/2/24.
+//  Copyright © 2016年 cdh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
